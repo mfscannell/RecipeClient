4f0cea9082b28f9f529d56eae3154eae854d8cd6
mfscannell <markfscannell@gmail.com>
2016-05-29 18:22:35 -0500

    First Commit.  Added splash page.

---
+++ app/src/main/java/com/scannell/mark/recipe/StartupActivity.java
@@ -0,0 +1,25 @@
+package com.scannell.mark.recipe;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * The startup page.
+ */
+public class StartupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}
