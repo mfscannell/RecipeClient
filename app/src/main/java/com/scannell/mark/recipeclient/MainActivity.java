0fde50c768428df82af9f249d3bd48e2fb22146f
mfscannell <markfscannell@gmail.com>
2016-06-21 12:53:46 -0500

    removed commented out code

--- app/src/main/java/com/scannell/mark/recipeclient/MainActivity.java
+++ app/src/main/java/com/scannell/mark/recipeclient/MainActivity.java
@@ -145,14 +145,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-//            switch (position) {
-//                case 0:
-//                    return "SECTION 1";
-//                case 1:
-//                    return "SECTION 2";
-//                case 2:
-//                    return "SECTION 3";
-//            }
             return null;
         }
     }
